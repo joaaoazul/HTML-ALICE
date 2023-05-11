cc771354f0cba0a9ecc1e25eebcf4285f9f4adfc
joaao <joao.azul.11@outlook.com>
2023-05-11 15:26:22 +0100

    working ticket before major changes

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ btn1.addEventListener('click', () => {
 });
 */
 
-const btn1 = document.getElementById('yas');
-const ticket = document.getElementById('ticket1');
+
+
+const btn1 = document.querySelector('#yas');
+const ticket = document.querySelector('#ticket1');
 
 btn1.addEventListener('click', () => {
   ticket.classList.toggle('hidden');
-  ticket.classList.toggle('ticket');
+  ticket.classList.toggle('ticket1');
 });
