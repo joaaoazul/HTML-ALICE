1e19086e7dc1be74939db83ee4b56bd37d307680
joaao <joao.azul.11@outlook.com>
2023-05-11 14:13:05 +0100

    changes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+    /* const btn1 = document.getElementById('yas');
+const ticket = document.getElementById('ticket1');
 
+btn1.addEventListener('click', () => {
+    ticket.style.opacity = 1;
+});
+*/
+
+const btn1 = document.getElementById('yas');
+const ticket = document.getElementById('ticket1');
+
+btn1.addEventListener('click', () => {
+  ticket.classList.toggle('hidden');
+  ticket.classList.toggle('ticket');
+});
